9220ba9918769cc117353abf66d17fecf94c29b3
Thomas Haller <thaller@redhat.com>
2024-06-14 18:30:13 +0200

    http: implement test_type=HTTP for fetching a small file via HTTP
    Inspired by ovn-kuber-traffic-flow-tests (client: [1], server: [2]).
    
    [1] https://github.com/Billy99/ovn-kuber-traffic-flow-tests/blob/17369edc2875b18c1c9df96683518cbc76600fe8/process-cmds.sh#L38
    [2] https://github.com/Billy99/ovn-kuber-traffic-flow-tests/blob/17369edc2875b18c1c9df96683518cbc76600fe8/manifests/http-server-pod-v4.yaml.j2#L36
    
    https://issues.redhat.com/browse/NHE-711

---
+++ testTypeHttp.py
@@ -0,0 +1,98 @@
+import json
+import shlex
+
+from dataclasses import dataclass
+
+import common
+import perf
+import tftbase
+
+from logger import logger
+from perf import PerfClient
+from perf import PerfServer
+from task import TaskOperation
+from testSettings import TestSettings
+from testType import TestTypeHandler
+from tftbase import BaseOutput
+from tftbase import IperfOutput
+from tftbase import TestType
+
+
+@dataclass(frozen=True)
+class TestTypeHandlerHttp(TestTypeHandler):
+    def __init__(self) -> None:
+        super().__init__(TestType.HTTP)
+
+    def _create_server_client(self, ts: TestSettings) -> tuple[PerfServer, PerfClient]:
+        s = HttpServer(ts=ts)
+        c = HttpClient(ts=ts, server=s)
+        return (s, c)
+
+
+test_type_handler_http = TestTypeHandlerHttp()
+
+
+class HttpServer(perf.PerfServer):
+    def cmd_line_args(self) -> list[str]:
+        return [
+            "-m",
+            "http.server",
+            "-d",
+            "/etc/ocp-traffic-flow-tests",
+            f"{self.port}",
+        ]
+
+    def get_template_args(self) -> dict[str, str]:
+
+        extra_args: dict[str, str] = {}
+        if self.exec_persistent:
+            extra_args["command"] = "python3"
+            extra_args["args"] = json.dumps(self.cmd_line_args())
+
+        return {
+            **super().get_template_args(),
+            **extra_args,
+        }
+
+    def _create_setup_operation_get_thread_action_cmd(self) -> str:
+        return f"python3 {shlex.join(self.cmd_line_args())}"
+
+    def _create_setup_operation_get_cancel_action_cmd(self) -> str:
+        return "killall python3"
+
+
+class HttpClient(perf.PerfClient):
+    def _create_task_operation(self) -> TaskOperation:
+        server_ip = self.get_target_ip()
+        cmd = f"exec {self.pod_name} -- curl --fail -s http://{server_ip}:{self.port}/data"
+
+        def _thread_action() -> BaseOutput:
+            self.ts.clmo_barrier.wait()
+            r = self.run_oc(cmd)
+            self.ts.event_client_finished.set()
+
+            return IperfOutput(
+                success=(
+                    r.success and r.out == "ocp-traffic-flow-tests\n" and r.err == ""
+                ),
+                tft_metadata=self.ts.get_test_metadata(),
+                command=cmd,
+                result={
+                    "result": common.dataclass_to_dict(r),
+                },
+            )
+
+        return TaskOperation(
+            log_name=self.log_name,
+            thread_action=_thread_action,
+        )
+
+    def _aggregate_output(
+        self,
+        result: tftbase.AggregatableOutput,
+        out: tftbase.TftAggregateOutput,
+    ) -> None:
+        assert isinstance(result, IperfOutput)
+
+        logger.info(f"Results of {self.ts.get_test_str()}")
+        out.flow_test = result
