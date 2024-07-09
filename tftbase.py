wizhaoredhat/ocp-traffic-flow-tests

commit 825800624bd63745b7e714fe7c04b48c95c224b5

all: allow overwriding the image pull police via environment variable
When making changes to the image, it can be important to force pulling the
latest image (TFT_TEST_IMAGE) from the repository.

Honor TFT_IMAGE_PULL_POLICY for that (for example, set it to "Always").
If unset, then we default to "IfNotPresent" unless "TFT_TEST_IMAGE"
environment variable is set.

tftbase.py
@@ -17,6 +17,7 @@ from logger import logger
 
 
 ENV_TFT_TEST_IMAGE = "TFT_TEST_IMAGE"
+ENV_TFT_IMAGE_PULL_POLICY = "TFT_IMAGE_PULL_POLICY"
 
 ENV_TFT_TEST_IMAGE_DEFAULT = "quay.io/wizhao/tft-tools:latest"
 
@@ -31,6 +32,30 @@ def get_environ(name: str) -> Optional[str]:
 def get_tft_test_image() -> str:
     s = get_environ(ENV_TFT_TEST_IMAGE) or ENV_TFT_TEST_IMAGE_DEFAULT
     logger.info(f"env: {ENV_TFT_TEST_IMAGE}={shlex.quote(s)}")
+    return s
+
+
+@functools.cache
+def get_tft_image_pull_policy() -> str:
+    s: Optional[str] = None
+    s_env = get_environ(ENV_TFT_IMAGE_PULL_POLICY)
+    if s_env is not None:
+        s0 = s_env.strip().lower()
+        if s0 == "always":
+            s = "Always"
+        if s0 == "ifnotpresent":
+            s = "IfNotPresent"
+        if s0 == "never":
+            s = "Never"
+        logger.error(
+            f'env: invalid environment variable in {ENV_TFT_IMAGE_PULL_POLICY}="{shlex.quote(s_env)}". Set to one of "IfNotPresent", "Always", "Never"'
+        )
+    if s is None:
+        if get_environ(ENV_TFT_TEST_IMAGE):
+            s = "Always"
+        else:
+            s = "IfNotPresent"
+    logger.info(f"env: {ENV_TFT_IMAGE_PULL_POLICY}={shlex.quote(s)}")
     return s
 
 

manifests/host-pod.yaml.j2
@@ -17,7 +17,7 @@ spec:
     command:
       - "{{ command }}"
     args: {{ args }}
-    imagePullPolicy: IfNotPresent
+    imagePullPolicy: {{ image_pull_policy }}
     securityContext:
       capabilities:
         add: ["NET_ADMIN","NET_RAW"]

manifests/pod.yaml.j2
@@ -15,4 +15,4 @@ spec:
     command:
       - "{{ command }}"
     args: {{ args }}
-    imagePullPolicy: IfNotPresent
+    imagePullPolicy: {{ image_pull_policy }}

manifests/sriov-pod.yaml.j2
@@ -17,4 +17,4 @@ spec:
     command:
       - "{{ command }}"
     args: {{ args }}
-    imagePullPolicy: IfNotPresent
+    imagePullPolicy: {{ image_pull_policy }}

manifests/tools-pod.yaml.j2
@@ -15,7 +15,7 @@ spec:
     command:
       - "{{ command }}"
     args: ["{{ args }}"]
-    imagePullPolicy: IfNotPresent
+    imagePullPolicy: {{ image_pull_policy }}
     securityContext:
       privileged: true
       runAsUser: 0

task.py
@@ -255,6 +255,7 @@ class Task(ABC):
         return {
             "name_space": self.get_namespace(),
             "test_image": tftbase.get_tft_test_image(),
+            "image_pull_policy": tftbase.get_tft_image_pull_policy(),
             "command": "/sbin/init",
             "args": "",
             "index": f"{self.index}",

commit 1eb78170f83137a1706c1e967e2150b9ab62cef0

tft: fix wrong logging in get_tft_image_pull_policy()
Fixes: 825800624bd6 ('all: allow overwriding the image pull police via environment variable')

tftbase.py
@@ -47,13 +47,14 @@ def get_tft_image_pull_policy() -> str:
         s0 = s_env.strip().lower()
         if s0 == "always":
             s = "Always"
-        if s0 == "ifnotpresent":
+        elif s0 == "ifnotpresent":
             s = "IfNotPresent"
-        if s0 == "never":
+        elif s0 == "never":
             s = "Never"
-        logger.error(
-            f'env: invalid environment variable in {ENV_TFT_IMAGE_PULL_POLICY}="{shlex.quote(s_env)}". Set to one of "IfNotPresent", "Always", "Never"'
-        )
+        else:
+            logger.error(
+                f'env: invalid environment variable in {ENV_TFT_IMAGE_PULL_POLICY}="{shlex.quote(s_env)}". Set to one of "IfNotPresent", "Always", "Never"'
+            )
     if s is None:
         if get_environ(ENV_TFT_TEST_IMAGE):
             s = "Always"