wizhaoredhat/ocp-traffic-flow-tests

commit 6e0e4f6221886ed47cb297e02c09ec1f5389b084

pluginValidateOffload: extract ethtool_stat_get_packets() and signal parse error
No longer emit exceptions when parsing invalid/unexpected ethtool data.
Instead, recognize the lack of the expected data, and forward the error
by setting "success" of the result to false.

An exception at this point is not a useful reaction. And we probably
should treat unexpected ethtool output not as a bug, but an error that
can happen at runtime (and needs to be handled). Handle the error by
setting the success to False, instead of emitting an exception.

Also, extract the code that parses "ethtool -S" output and move it
separate functions. Also add a unit tests.

Note that case2 (fallback to X_queue_..._packets) is not unit tested,
because I don't have such an output at hand. Also, the original code
searches for "_xdp_packets" suffix, but it's not clear to me where this
comes from (searching the kernel sources does not indicate that drivers
return such a field). In any case, when you encounter this case in the
wild, extend the unit test.

pluginValidateOffload.py
@@ -21,6 +21,78 @@ from tftbase import TestMetadata
 VF_REP_TRAFFIC_THRESHOLD = 1000
 
 
+def ethtool_stat_parse(output: str) -> dict[str, str]:
+    result = {}
+    for line in output.splitlines():
+        try:
+            key, val = line.split(":", 2)
+        except Exception:
+            continue
+        if val == "" and " " in key:
+            # This is a section heading.
+            continue
+        result[key.strip()] = val.strip()
+    return result
+
+
+def ethtool_stat_get_packets(data: dict[str, str], packet_type: str) -> Optional[int]:
+
+    # Case1: Try to parse rx_packets and tx_packets from ethtool output
+    val = data.get(f"{packet_type}_packets")
+    if val is not None:
+        try:
+            return int(val)
+        except KeyError:
+            return None
+
+    # Case2: Ethtool output does not provide these fields, so we need to sum
+    # the queues manually.
+    total_packets = 0
+    prefix = f"{packet_type}_queue_"
+    packet_suffix = "_xdp_packets"
+    any_match = False
+
+    for k, v in data.items():
+        if k.startswith(prefix) and k.endswith(packet_suffix):
+            try:
+                total_packets += int(v)
+            except KeyError:
+                return None
+            any_match = True
+    if not any_match:
+        return None
+    return total_packets
+
+
+def ethtool_stat_get_startend(
+    parsed_data: dict[str, int],
+    ethtool_data: str,
+    suffix: typing.Literal["start", "end"],
+) -> bool:
+    ethtool_dict = ethtool_stat_parse(ethtool_data)
+    success = True
+    KEY_NAMES = {
+        "start": {
+            "rx": "rx_start",
+            "tx": "tx_start",
+        },
+        "end": {
+            "rx": "rx_end",
+            "tx": "tx_end",
+        },
+    }
+    for ethtool_name in ("rx", "tx"):
+        # Don't construct key_name as f"{ethtool_name}_{suffix}", because the
+        # keys should appear verbatim in source code, so we can grep for them.
+        key_name = KEY_NAMES[suffix][ethtool_name]
+        v = ethtool_stat_get_packets(ethtool_dict, ethtool_name)
+        if v is None:
+            success = False
+            continue
+        parsed_data[key_name] = v
+    return success
+
+
 def no_traffic_on_vf_rep(
     rx_start: int, tx_start: int, rx_end: int, tx_end: int
 ) -> bool:
@@ -52,26 +124,17 @@ class PluginValidateOffload(pluginbase.Plugin):
     def eval_log(
         self, plugin_output: PluginOutput, md: TestMetadata
     ) -> Optional[PluginResult]:
-        rx_start = plugin_output.result.get("rx_start")
-        tx_start = plugin_output.result.get("tx_start")
-        rx_end = plugin_output.result.get("rx_end")
-        tx_end = plugin_output.result.get("tx_end")
-
-        if any(x is None for x in [rx_start, tx_start, rx_end, tx_end]):
+        if not plugin_output.success:
             logger.error(
                 f"Validate offload plugin is missing expected ethtool data in {md.test_case_id}"
             )
             success = False
         else:
-            assert isinstance(rx_start, int)
-            assert isinstance(tx_start, int)
-            assert isinstance(rx_end, int)
-            assert isinstance(tx_end, int)
             success = no_traffic_on_vf_rep(
-                rx_start=rx_start,
-                tx_start=tx_start,
-                rx_end=rx_end,
-                tx_end=tx_end,
+                rx_start=plugin_output.result_get("rx_start", int),
+                tx_start=plugin_output.result_get("tx_start", int),
+                rx_end=plugin_output.result_get("rx_end", int),
+                tx_end=plugin_output.result_get("tx_end", int),
             )
 
         return PluginResult(
@@ -150,27 +213,6 @@ class TaskValidateOffload(PluginTask):
             success = r.success or ("already exists" not in r.err)
         return success, r
 
-    def parse_packets(self, output: str, packet_type: str) -> int:
-        # Case1: Try to parse rx_packets and tx_packets from ethtool output
-        prefix = f"{packet_type}_packets"
-        if prefix in output:
-            for line in output.splitlines():
-                stripped_line = line.strip()
-                if stripped_line.startswith(prefix):
-                    return int(stripped_line.split(":")[1])
-        # Case2: Ethtool output does not provide these fields, so we need to sum the queues manually
-        total_packets = 0
-        prefix = f"{packet_type}_queue_"
-        packet_suffix = "_xdp_packets:"
-
-        for line in output.splitlines():
-            stripped_line = line.strip()
-            if prefix in stripped_line and packet_suffix in stripped_line:
-                packet_count = int(stripped_line.split(":")[1].strip())
-                total_packets += packet_count
-
-        return total_packets
-
     def _create_task_operation(self) -> TaskOperation:
         def _thread_action() -> BaseOutput:
             self.ts.clmo_barrier.wait()
@@ -197,15 +239,13 @@ class TaskValidateOffload(PluginTask):
             if success2 and r2.success:
                 data2 = r2.out
 
-            parsed_data: dict[str, str | int] = {}
+            success_result = success2 and r2.success
 
-            if data1:
-                parsed_data["rx_start"] = self.parse_packets(data1, "rx")
-                parsed_data["tx_start"] = self.parse_packets(data1, "tx")
-
-            if data2:
-                parsed_data["rx_end"] = self.parse_packets(data2, "rx")
-                parsed_data["tx_end"] = self.parse_packets(data2, "tx")
+            parsed_data: dict[str, int] = {}
+            if not ethtool_stat_get_startend(parsed_data, data1, "start"):
+                success_result = False
+            if not ethtool_stat_get_startend(parsed_data, data2, "end"):
+                success_result = False
 
             logger.info(
                 f"rx_packet_start: {parsed_data.get('rx_start', 'N/A')}\n"
@@ -214,7 +254,7 @@ class TaskValidateOffload(PluginTask):
                 f"tx_packet_end: {parsed_data.get('tx_end', 'N/A')}\n"
             )
             return PluginOutput(
-                success=success2 and r2.success,
+                success=success_result,
                 command=ethtool_cmd,
                 plugin_metadata={
                     "name": "GetEthtoolStats",

tests/test_pluginValidateOffload.py
@@ -1,0 +1,64 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pluginValidateOffload  # noqa: E402
+
+
+def test_ethtool_parse_stat() -> None:
+
+    assert pluginValidateOffload.ethtool_stat_parse("") == {}
+
+    data = """NIC statistics:
+     tx_packets: 2537925
+     rx_packets: 5645343
+     tx_errors: 0
+     rx_errors: 0
+     rx_missed: 0
+     align_errors: 0
+     tx_single_collisions: 0
+     tx_multi_collisions: 0
+     rx_unicast: 5373570
+     rx_broadcast: 226191
+     rx_multicast: 45582
+     tx_aborted: 0
+     tx_underrun: 0
+"""
+    expected_d = {
+        "tx_packets": "2537925",
+        "rx_packets": "5645343",
+        "tx_errors": "0",
+        "rx_errors": "0",
+        "rx_missed": "0",
+        "align_errors": "0",
+        "tx_single_collisions": "0",
+        "tx_multi_collisions": "0",
+        "rx_unicast": "5373570",
+        "rx_broadcast": "226191",
+        "rx_multicast": "45582",
+        "tx_aborted": "0",
+        "tx_underrun": "0",
+    }
+
+    d = pluginValidateOffload.ethtool_stat_parse(data)
+    assert d == expected_d
+    assert list(d) == list(expected_d)
+
+    assert pluginValidateOffload.ethtool_stat_get_packets(d, "tx") == 2537925
+    assert pluginValidateOffload.ethtool_stat_get_packets(d, "rx") == 5645343
+    assert pluginValidateOffload.ethtool_stat_get_packets(d, "foo") is None
+
+    res: dict[str, int] = {}
+    assert pluginValidateOffload.ethtool_stat_get_startend(res, data, "start")
+    assert res == {
+        "rx_start": 5645343,
+        "tx_start": 2537925,
+    }
+    assert pluginValidateOffload.ethtool_stat_get_startend(res, data, "end")
+    assert res == {
+        "rx_start": 5645343,
+        "tx_start": 2537925,
+        "rx_end": 5645343,
+        "tx_end": 2537925,
+    }

commit 12cec0d15d5f8bf8c2910166aec0f3a12c213d2b

validate_offload: fix parsing ethtool result and acceptin missing TX/RX
Before commit 6e0e4f622188 ('pluginValidateOffload: extract
ethtool_stat_get_packets() and signal parse error'), missing data would
be interpreted as 0 (by parse_packets()). The commit tightened that up,
but there are legitimate(?) results that only have the RX values but not
TX (see unit test).

Relax the error checking when parsing the result of validate_offload
plugin.

Fixes: 6e0e4f622188 ('pluginValidateOffload: extract ethtool_stat_get_packets() and signal parse error')

pluginValidateOffload.py
@@ -62,42 +62,54 @@ def ethtool_stat_get_packets(data: dict[str, str], packet_type: str) -> Optional
     return total_packets
 
 
+KEY_NAMES = {
+    "start": {
+        "rx": "rx_start",
+        "tx": "tx_start",
+    },
+    "end": {
+        "rx": "rx_end",
+        "tx": "tx_end",
+    },
+}
+
+
 def ethtool_stat_get_startend(
     parsed_data: dict[str, int],
     ethtool_data: str,
     suffix: typing.Literal["start", "end"],
 ) -> bool:
     ethtool_dict = ethtool_stat_parse(ethtool_data)
-    success = True
-    KEY_NAMES = {
-        "start": {
-            "rx": "rx_start",
-            "tx": "tx_start",
-        },
-        "end": {
-            "rx": "rx_end",
-            "tx": "tx_end",
-        },
-    }
+    has_any = False
     for ethtool_name in ("rx", "tx"):
         # Don't construct key_name as f"{ethtool_name}_{suffix}", because the
         # keys should appear verbatim in source code, so we can grep for them.
         key_name = KEY_NAMES[suffix][ethtool_name]
         v = ethtool_stat_get_packets(ethtool_dict, ethtool_name)
         if v is None:
-            success = False
             continue
         parsed_data[key_name] = v
-    return success
+        has_any = True
+    return has_any
 
 
-def no_traffic_on_vf_rep(
-    rx_start: int, tx_start: int, rx_end: int, tx_end: int
-) -> bool:
-    return (
-        rx_end - rx_start < VF_REP_TRAFFIC_THRESHOLD
-        and tx_end - tx_start < VF_REP_TRAFFIC_THRESHOLD
+def check_no_traffic_on_vf_rep(
+    parsed_data: dict[str, typing.Any],
+    direction: typing.Literal["rx", "tx"],
+) -> Optional[str]:
+    start = common.dict_get_typed(
+        parsed_data, KEY_NAMES["start"][direction], int, allow_missing=True
     )
+    end = common.dict_get_typed(
+        parsed_data, KEY_NAMES["end"][direction], int, allow_missing=True
+    )
+    if start is None or end is None:
+        if start is not None or end is not None:
+            return f"missing ethtool output for {direction}"
+        return None
+    if end - start >= VF_REP_TRAFFIC_THRESHOLD:
+        return "traffic on VF rep detected"
+    return None
 
 
 class PluginValidateOffload(pluginbase.Plugin):
@@ -218,13 +230,19 @@ class TaskValidateOffload(PluginTask):
 
                     if not r1.success:
                         success_result = False
-                    if not r2.success:
+                        msg = "ethtool command failed"
+                    elif not r2.success:
                         success_result = False
+                        msg = "ethtool command at end failed"
 
                     if not ethtool_stat_get_startend(parsed_data, data1, "start"):
-                        success_result = False
+                        if success_result:
+                            success_result = False
+                            msg = "ethtool output cannot be parsed"
                     if not ethtool_stat_get_startend(parsed_data, data2, "end"):
-                        success_result = False
+                        if success_result:
+                            success_result = False
+                            msg = "ethtool output at end cannot be parsed"
 
                 logger.info(
                     f"rx_packet_start: {parsed_data.get('rx_start', 'N/A')}\n"
@@ -234,14 +252,11 @@ class TaskValidateOffload(PluginTask):
                 )
 
                 if success_result:
-                    if not no_traffic_on_vf_rep(
-                        rx_start=common.dict_get_typed(parsed_data, "rx_start", int),
-                        tx_start=common.dict_get_typed(parsed_data, "tx_start", int),
-                        rx_end=common.dict_get_typed(parsed_data, "rx_end", int),
-                        tx_end=common.dict_get_typed(parsed_data, "tx_end", int),
-                    ):
+                    m1 = check_no_traffic_on_vf_rep(parsed_data, "rx")
+                    m2 = check_no_traffic_on_vf_rep(parsed_data, "tx")
+                    if m1 is not None or m2 is not None:
                         success_result = False
-                        msg = "no traffic on VF rep detected"
+                        msg = m1 if m1 is not None else m2
 
             return PluginOutput(
                 success=success_result,

tests/test_pluginValidateOffload.py
@@ -1897,26 +1897,22 @@ def test_ethtool_parse_stat_2() -> None:
     assert v is None
 
     res1: dict[str, int] = {}
-    assert not pluginValidateOffload.ethtool_stat_get_startend(
-        res1, data1_start, "start"
-    )
+    assert pluginValidateOffload.ethtool_stat_get_startend(res1, data1_start, "start")
     assert res1 == {
         "rx_start": 7,
     }
-    assert not pluginValidateOffload.ethtool_stat_get_startend(res1, data1_end, "end")
+    assert pluginValidateOffload.ethtool_stat_get_startend(res1, data1_end, "end")
     assert res1 == {
         "rx_start": 7,
         "rx_end": 7,
     }
 
     res2: dict[str, int] = {}
-    assert not pluginValidateOffload.ethtool_stat_get_startend(
-        res2, data2_start, "start"
-    )
+    assert pluginValidateOffload.ethtool_stat_get_startend(res2, data2_start, "start")
     assert res2 == {
         "rx_start": 7,
     }
-    assert not pluginValidateOffload.ethtool_stat_get_startend(res2, data2_end, "end")
+    assert pluginValidateOffload.ethtool_stat_get_startend(res2, data2_end, "end")
     assert res2 == {
         "rx_start": 7,
         "rx_end": 7,