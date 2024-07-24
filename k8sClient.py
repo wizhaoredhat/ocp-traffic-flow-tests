aed07491b3481676f57e91ce7697a914d986c7d9
Thomas Haller <thaller@redhat.com>
2024-07-24 08:27:09 +0200

    k8sClient,task: add K8sClient.oc_get() and Task.run_oc_get() helpers

--- k8sClient.py
+++ k8sClient.py
@@ -1,13 +1,17 @@
+import json
 import kubernetes  # type: ignore
 import logging
 import os
 import shlex
+import sys
 import typing
 import yaml
 
 from collections.abc import Iterable
 
 import host
+
+from logger import logger
 
 
 class K8sClient:
@@ -45,6 +49,25 @@ class K8sClient:
             return shlex.split(cmd)
         return list(cmd)
 
+    def _oc_get_full_cmd(
+        self,
+        *,
+        cmd: str | Iterable[str],
+        namespace: typing.Optional[str] = None,
+    ) -> list[str]:
+        namespace_args: tuple[str, ...]
+        if namespace:
+            namespace_args = ("-n", namespace)
+        else:
+            namespace_args = ()
+        return [
+            "kubectl",
+            "--kubeconfig",
+            self._kc,
+            *namespace_args,
+            *self._oc_get_cmd(cmd),
+        ]
+
     def oc(
         self,
         cmd: str | Iterable[str],
@@ -53,19 +76,8 @@ class K8sClient:
         die_on_error: bool = False,
         namespace: typing.Optional[str] = None,
     ) -> host.Result:
-        namespace_args: tuple[str, ...]
-        if namespace:
-            namespace_args = ("-n", namespace)
-        else:
-            namespace_args = ()
         return host.local.run(
-            [
-                "kubectl",
-                "--kubeconfig",
-                self._kc,
-                *namespace_args,
-                *self._oc_get_cmd(cmd),
-            ],
+            self._oc_get_full_cmd(cmd=cmd, namespace=namespace),
             die_on_error=die_on_error,
             log_level_fail=logging.DEBUG if may_fail else logging.ERROR,
         )
@@ -85,3 +97,43 @@ class K8sClient:
             die_on_error=die_on_error,
             namespace=namespace,
         )
+
+    def oc_get(
+        self,
+        what: str,
+        *,
+        may_fail: bool = False,
+        die_on_error: bool = False,
+        namespace: typing.Optional[str] = None,
+    ) -> typing.Optional[dict[str, typing.Any]]:
+        cmd = ["get", what, "-o", "json"]
+        ret = self.oc(
+            cmd,
+            may_fail=may_fail,
+            die_on_error=die_on_error,
+            namespace=namespace,
+        )
+
+        if not ret.success:
+            # No need for extra logging in this failure case. self.oc() already
+            # did all the logging we want.
+            return None
+
+        try:
+            data = json.loads(ret.out)
+        except ValueError:
+            data = None
+
+        if not isinstance(data, dict):
+            cmd_s = shlex.join(self._oc_get_full_cmd(cmd=cmd, namespace=namespace))
+            if not may_fail or die_on_error:
+                logger.error(
+                    f"Command {cmd_s} did not return a JSON dictionary but {ret.debug_str()}"
+                )
+            else:
+                logger.debug(f"Command {cmd_s} did not return a JSON dictionary")
+            if die_on_error:
+                sys.exit(-1)
+            return None
+
+        return data
